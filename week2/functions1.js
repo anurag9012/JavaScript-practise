60ec37255921d1b1f528dcdca595cdb7c9343f16
anurag9012 <anurag9012@gmail.com>
2023-02-03 13:32:47 +0530

    first commit

---
+++ week2/functions1.js
@@ -0,0 +1,14 @@
+
+// function add2numbers(a,b){
+//     var c=a+b;
+//     console.log(c);
+// }
+// add2numbers(3,5);
+
+var colors = ['red', 'orange', 'yellow', 'green', 'blue', 'purple', 'pink'];
+function printarray(arr){
+for (var i=0;i<arr.length;i++){
+    console.log(i+1,arr[i]);
+}
+}
+printarray(colors)
