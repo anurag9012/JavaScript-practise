f32aa6fcdc8a6f9e4da3c8ddc0a630137d792f77
anurag9012 <anurag9012@gmail.com>
2023-02-03 21:11:24 +0530

    starting form validation

--- week2/functions1.js
+++ week2/functions1.js
@@ -8,13 +8,13 @@
 
 /* FUNCTION TO PRINT AN ARRAY */
 
-/* var colors = ['red', 'orange', 'yellow', 'green', 'blue', 'purple', 'pink'];
+var colors = ['red', 'orange', 'yellow', 'green', 'blue', 'purple', 'pink'];
 function printarray(arr){
 for (var i=0;i<arr.length;i++){
     console.log(i+1,arr[i]);
 }
 }
-printarray(colors) */
+//printarray(colors)
 
 
 function letterFinder(word, match){
@@ -29,5 +29,20 @@ function letterFinder(word, match){
          else {console.log("no match found at", i)}
     }
 }
-letterFinder("test","t");
+//letterFinder("test","t");
 
+/* OBJECTS 
+THEY ARE a way of storing info wherethe information  are related to one thing , in the form of property and property value....
+*/
+//Object literals and the brackets notation
+var house={}
+house.doors=3;
+house.bathroom=4;
+house["floors"]=2;
+console.log(house)
+
+
+//In JavaScript, arrays are objects. That means that arrays also have some built-in properties and methods.
+var random=Math.random()*10;
+console.log(random);
+console.log(Math.ceil(random)-1);
