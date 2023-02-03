d8133a75dd3459e2de55d7d2b5d8faf50bf75f88
anurag9012 <anurag9012@gmail.com>
2023-02-03 13:54:16 +0530

    second commit

--- week2/functions1.js
+++ week2/functions1.js
@@ -5,10 +5,29 @@
 // }
 // add2numbers(3,5);
 
-var colors = ['red', 'orange', 'yellow', 'green', 'blue', 'purple', 'pink'];
+
+/* FUNCTION TO PRINT AN ARRAY */
+
+/* var colors = ['red', 'orange', 'yellow', 'green', 'blue', 'purple', 'pink'];
 function printarray(arr){
 for (var i=0;i<arr.length;i++){
     console.log(i+1,arr[i]);
 }
 }
-printarray(colors)
+printarray(colors) */
+
+
+function letterFinder(word, match){
+    
+    // let arr=word.split("");
+    // for (var i=0;i<arr.length;i++){
+    //     if (arr[i]==match){console.log("found the ",match,"at",i)}
+    //     else {console.log("no match found at", i)}
+    // }
+    for (var i=0;i<word.length;i++){
+        if (word[i]==match){console.log("found the ",match,"at",i)}
+         else {console.log("no match found at", i)}
+    }
+}
+letterFinder("test","t");
+
